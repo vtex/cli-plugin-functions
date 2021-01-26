46eb1c044c235ca990f9b827b6c0d1d54c422d0a
Rodrigo Orem <orem.rodrigo@gmail.com>
2021-01-26 16:21:43 -0300

    Better naming for comamnds classes and lint errors

--- src/commands/functions/deploy.ts
+++ src/commands/functions/deploy.ts
@@ -1,6 +1,6 @@
 import { Command } from '@oclif/command'
 
-export default class Functions extends Command {
+export default class FunctionsDeploy extends Command {
   static description = 'upload serverless functions to the cloud'
 
   static examples = [`$ vtex-test functions deploy`]
