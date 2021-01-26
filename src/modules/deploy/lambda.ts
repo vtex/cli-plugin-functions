341087d1e9f47945950beaa0d00b4f33c48c42e7
Rodrigo Orem <orem.rodrigo@gmail.com>
2021-01-26 17:03:54 -0300

    Create function on toolbelt command
    It will deploy a function.zip on you current working directory after
    running `vtex-test function deploy`.

--- src/modules/deploy/lambda.ts
+++ src/modules/deploy/lambda.ts
@@ -3,26 +3,28 @@ import AWS from 'aws-sdk'
 
 AWS.config.update({ region: 'us-east-2' })
 
-const lambda = new AWS.Lambda()
+export const createFunction = () => {
+  const lambda = new AWS.Lambda()
 
-const params = {
-  Code: {
-    ZipFile: fs.readFileSync('./function.zip'),
-  },
-  Description: 'Test function in nodejs deployed using nodejs',
-  FunctionName: 'hello-node-1',
-  Handler: 'index.handler',
-  Publish: true,
-  Role: 'arn:aws:iam::688157472274:role/lambda-ex',
-  Runtime: 'nodejs12.x',
-  Timeout: 15,
-  TracingConfig: {
-    Mode: 'Active',
-  },
+  const params = {
+    Code: {
+      ZipFile: fs.readFileSync('./function.zip'),
+    },
+    Description: 'Test function in nodejs deployed using nodejs',
+    FunctionName: 'hello-node-1',
+    Handler: 'index.handler',
+    Publish: true,
+    Role: 'arn:aws:iam::688157472274:role/lambda-ex',
+    Runtime: 'nodejs12.x',
+    Timeout: 15,
+    TracingConfig: {
+      Mode: 'Active',
+    },
+  }
+
+  lambda.createFunction(params, function handler(err, data) {
+    if (err) console.log(err, err.stack)
+    // an error occurred
+    else console.log(data) // successful response
+  })
 }
-
-lambda.createFunction(params, function handler(err, data) {
-  if (err) console.log(err, err.stack)
-  // an error occurred
-  else console.log(data) // successful response
-})
