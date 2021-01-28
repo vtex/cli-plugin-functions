9a224f6e9e60b3243fa3cf31a0d461b651eb9fa9
Rodrigo Orem <orem.rodrigo@gmail.com>
2021-01-28 02:12:26 -0300

    Create API Gateway and Resource on AWS

--- src/modules/deploy/lambda.ts
+++ src/modules/deploy/lambda.ts
@@ -3,7 +3,7 @@ import AWS from 'aws-sdk'
 
 AWS.config.update({ region: 'us-east-2' })
 
-export const createFunction = () => {
+export const createFunction = async () => {
   const lambda = new AWS.Lambda()
 
   const params = {
@@ -22,9 +22,36 @@ export const createFunction = () => {
     },
   }
 
-  lambda.createFunction(params, function handler(err, data) {
-    if (err) console.log(err, err.stack)
-    // an error occurred
-    else console.log(data) // successful response
+  const apigateway = new AWS.APIGateway()
+
+  const functionResp = await lambda.createFunction(params).promise()
+
+  console.log('function', functionResp)
+
+  const apiParams = {
+    name: 'hello-node-1-rest-api',
+  }
+
+  const respRestApi = await apigateway.createRestApi(apiParams).promise()
+
+  console.log('rest api', respRestApi)
+
+  const resources = await apigateway.getResources({ restApiId: respRestApi.id! }).promise()
+
+  /*
+  apigateway.getRestApis({ }, function(_error, data) {
+    console.dir(data, {depth: null})
+    console.dir(_error, {depth: null})
   })
+  */
+
+  const resourceParams = {
+    parentId: resources.items![0].id!,
+    pathPart: 'hello',
+    restApiId: respRestApi.id!,
+  }
+
+  const resourceResp = await apigateway.createResource(resourceParams).promise()
+
+  console.log('resource resp', resourceResp)
 }
