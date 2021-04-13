1ef28df941e38d832851c429632c87390c806198
Rodrigo Orem <orem.rodrigo@gmail.com>
2021-04-13 17:18:58 -0300

    Remove deploy code

--- src/modules/deploy/webpack.config.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseProvider } from './provider/BaseProvider'
-
-declare const generateConfig: (dirName: string, distDir: string, provider: BaseProvider) => Record<string, any>
-
-export { generateConfig }
